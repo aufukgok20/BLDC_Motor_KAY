b7d09d4979f3d896a9d6992e60846dafa1cbcac2
ahmetufukgok <ahmetufukgok@gmail.com>
2022-10-02 10:54:04 +0300

    v0_0_1_BLDC_Motor_KAY
    Microsoft Visual Studio 2022 - Windows Form App kullanilarak BLDC_Motor_KAY projesi olusturuldu.
    Microsoft Visual Studio Version = 17.3.32922.545
    Microsoft .NET Framework Version = 4.8.04084 (.NET 6.0 Long-term support)
    C# Tools Version = 4.3.0-3.22470.13

---
+++ BLDC_Motor_KAY/BLDC_Motor_KAY/Form1.cs
@@ -0,0 +1,15 @@
+namespace BLDC_Motor_KAY
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "BLDC_Motor_KAY";
+        }
+    }
+}
